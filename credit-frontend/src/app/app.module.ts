d7808f910a749fa2effa4d91eb2aabd6d59fc28f
Alejandro <handaijandro@gmail.com>
2021-02-22 09:29:18 +0900

    migrated project from css to scss

--- credit-frontend/src/app/app.module.ts
+++ credit-frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
